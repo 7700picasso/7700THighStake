feafff653046d95ec530743f8189c07c8105ac4f
7700A <94393717+7700picasso@users.noreply.github.com>
2025-01-09 19:31:29 -0800

    1/9/25 (auton skills - PinchDrive2)

--- 7700T_Drive/src/7700THighStake.cpp
+++ 7700T_Drive/src/7700THighStake.cpp
@@ -26,6 +26,8 @@ motor LB = motor(PORT7, ratio6_1, true);
 motor RB = motor(PORT2, ratio6_1, false);
 motor intake = motor(PORT16, ratio18_1, true);
 motor conveyorBelt = motor(PORT12, ratio18_1, false);
+motor ladybrown = motor(PORT4, ratio18_1, true);
+motor ladybrown2 = motor(PORT8, ratio18_1, false);
 digital_out clamp1(Brain.ThreeWirePort.A);
 digital_out doinker1(Brain.ThreeWirePort.B);
 inertial Gyro1 = inertial(PORT13);
@@ -40,9 +42,6 @@ void stopDrive(){
   RM.stop(brake);
 }
 
-void GyroPrint(){
-  Brain.Screen.printAt(10, 20, "Heading = %0.2f", Gyro1.rotation(deg));
-}
 
 void mogoClamp(){ 
   clamp1.set(true);
@@ -123,7 +122,6 @@ void display(){
 }
 
 void PinchDrive(float target){
-  // 1 inch off
   LM.setPosition(0, rev);
   float x = pi*LM.position(rev)*gr*Dia;
   float error = target - x;
@@ -143,53 +141,27 @@ void GyroTurn(float target){
   float theta = 0.0;
   float error = target - theta;
   float accuracy = 1.75;
-  float kp = 0.75; 
+  float kp = 0.75;
   float speed = kp*error;
   Gyro1.setRotation(0.0, deg);
-  while(fabs(error) > accuracy){ 
-    GyroPrint();
-    time_drive(speed *(error)/error, -speed*(error)/error, 10);
+  while(fabs(error) >= accuracy){
+    speed = kp*error;
+    time_drive(speed, -speed, 10);
     theta = Gyro1.rotation();
     error = target - theta;
-    speed = kp*error;
   }
   stopDrive();
-
 }
-
-void PinchDrive2(float target){
-  // adjusts by itself
-  float x = 0.0;
-  float error = target - x;
-  float speed = 75.0;
-  float accuracy = 0.5;
-  float ks = 3.0;
-  float yaw = 0.0;
-  float lspeed = speed * fabs(error) / error - ks * yaw;
-  float rspeed = speed * fabs(error) / error + ks * yaw;
-  Gyro1.setRotation(0.0, deg);
-  LB.setPosition(0, rev);
-  while(fabs(error) > accuracy) {
-    time_drive(lspeed, rspeed, 10);
-    x = LB.position(rev) * pi * Dia * gr;
-    error = target - x;
-    yaw = Gyro1.rotation(deg);
-    lspeed = speed * fabs(error)/error - ks * yaw;
-    rspeed = speed * fabs(error) / error + ks * yaw;
-  }
-}
-
-
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-//Gyro1.calibrate();
+Gyro1.calibrate();
 
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-  while(Gyro1.isCalibrating()){                                  
-    wait(200, msec);
+  while(Gyro1.isCalibrating()){
+    wait(10, msec);
     // :)
   }
 }
@@ -205,196 +177,51 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {   
-// ..........................................................................
+  // ..........................................................................
 
-// GyroTurn(360);
-// Brain.Screen.printAt(10, 50, "Heading = %0.2f", Gyro1.rotation(deg));
-// GyroTurn testing
-/*GyroTurn(90);
-PinchDrive(48);*/
-
-// PinchDrive testing
-//PinchDrive(3);
-
-// auto skills (alliance stakes)
-/*mogoUnclamp();
-PinchDrive(-3);
-mogoClamp();
-wait(250, msec);
-conveyorBelt.spin(fwd, 100, pct);
-GyroTurn(120);
-PinchDrive(48);
-GyroTurn(130);
-PinchDrive(-35);
-mogoUnclamp();
-PinchDrive(20);
-GyroTurn(-90);
-PinchDrive(-100);
-mogoClamp();
-wait(250, msec);
-GyroTurn(180);
-PinchDrive(40);
-GyroTurn(-90);=09
-PinchDrive(40);
-mogoUnclamp();*/
-
-// auto skills (normal/no alliance stakes) not finished
-//PinchDrive(-5);
-
-
-// auto skills from 1st comp day
-/*mogoUnclamp();
-PinchDrive(-27);
-mogoClamp();
-time_drive(50, -50, 230);
-PinchDrive(-30);*/
-
-/*// fixing one
-// first corner
-mogoUnclamp();
-PinchDrive(-8.253);
-mogoClamp();
-GyroTurn(115);
-//turn to get L
-conveyorBelt.spin(fwd, 100, pct);
-//score preload
-intake.spin(fwd, 100, pct);
-wait(250, msec);
-PinchDrive(41);
-//collect two rings from L
-PinchDrive(-5);
-GyroTurn(111);
-PinchDrive(-10);
-//collect two rings from L
-wait(250, msec);
-mogoUnclamp();
-//put in corner
-
-//second corner
-wait(100,msec);
-PinchDrive(10);
-GyroTurn(-123.5); //-125
-time_drive(60, 60, 750);
-//get ready to go to clamp second mogo
-PinchDrive(-83);
-mogoClamp();
-//clamp onto second mogo
-GyroTurn(174);
-//get ready to collect second L
-PinchDrive(44);
-PinchDrive(-8.5);
-//collect second L
-GyroTurn(-130);
-PinchDrive(-10);
-mogoUnclamp();
-//go into corner
-
-// push right side mobile goal into corner
-PinchDrive(11.5);
-GyroTurn(85);
-PinchDrive(108);
+  // testing 
+  /*GyroTurn(90);
+  wait(200, msec);
+  PinchDrive(24); 
 */
+  // 15-sec autonomous
+ /*mogoUnclamp();
+  PinchDrive(-27);
+  mogoClamp();
+  wait(250, msec);
+  intake.spin(fwd, 100, pct);
+  conveyorBelt.spin(fwd, 100, pct);
+  wait(1.5, sec);
+  conveyorBelt.spinToPosition(220, deg);   
+  wait(0.5, sec);
+  GyroTurn(190);
+  wait(200, msec);
+  PinchDrive(22);*/
+   
 
-// new align
-// fixing one
-// first corner
-mogoUnclamp();
-PinchDrive(-10);
-mogoClamp();
-GyroTurn(112);
-//turn to get L
-conveyorBelt.spin(fwd, 100, pct);
-//score preload
-intake.spin(fwd, 100, pct);
-wait(250, msec);
-time_drive(40,40,1700);
-intake.spin(fwd, 0, pct);
-time_drive(30,30,400);
-intake.spin(fwd, 100, pct);
-conveyorBelt.spin(fwd, 100, pct);
-// PinchDrive(41);
-//collect two rings from L
-PinchDrive(-5);
-GyroTurn(111);
-PinchDrive(-7.5);
-//collect two rings from L
-wait(250, msec);
-mogoUnclamp();
-//put in corner
-
-//second corner
-wait(100,msec);
-PinchDrive(7.5);
-GyroTurn(-122); //-125
-intake.spin(fwd, 0, pct);
-time_drive(60, 60, 700);
-intake.spin(fwd, 100, pct);
-//get ready to go to clamp second mogo
-PinchDrive2(-83); // PinchDrive or PinchDrive2
-mogoClamp();
-//clamp onto second mogo
-GyroTurn(174);
-//get ready to collect second L
-intake.spin(fwd, 100, pct);
-wait(250, msec);
-time_drive(40,40,1700);
-intake.spin(fwd, 0, pct);
-time_drive(30,30,400);
-intake.spin(fwd, 100, pct);
-conveyorBelt.spin(fwd, 100, pct);
-PinchDrive(-5);
-//PinchDrive(44);
-//PinchDrive(-8.5);
-//collect second L
-GyroTurn(-130);
-PinchDrive(-10);
-mogoUnclamp();
-//go into corner
-
-// push right side mobile goal into corner
-PinchDrive(11.5);
-GyroTurn(85);
-PinchDrive(108);
-
-// old one
-// // first corner
-// mogoUnclamp();
-// PinchDrive(-7.75);
-// mogoClamp();
-// GyroTurn(120);
-// //turn to get L
-// conveyorBelt.spin(fwd, 100, pct);
-// //score preload
-// intake.spin(fwd, 100, pct);
-// wait(120, msec);
-// PinchDrive(38.5);
-// //collect two rings from L
-// GyroTurn(112);
-// PinchDrive(-6.5);
-// wait(250, msec);
-// mogoUnclamp();
-// //put in corner
-
-// fart edition
-// //second corner
-// wait(100,msec);
-// PinchDrive(10.2);
-// GyroTurn(-116);
-// //get ready to go to clamp second mogo
-// PinchDrive(-76.5);
-// mogoClamp();
-// //clamp onto second mogo
-// GyroTurn(174);
-// //get ready to collect second L
-// PinchDrive(44);
-// PinchDrive(-10);
-// //collect second L
-// GyroTurn(-130);
-// PinchDrive(-10);
-// //go into corner
-
-
-
+  // touch bar autonomous
+ mogoUnclamp();
+  PinchDrive(-30);
+  mogoClamp();
+  wait(250, msec);
+  intake.spin(fwd, 100, pct);
+  conveyorBelt.spin(fwd, 100, pct);
+  wait(1.5, sec);
+  GyroTurn(100);
+  wait(300, msec);
+ PinchDrive(23.5);
+  intake.spin(fwd, 0, pct);
+  conveyorBelt.spin(fwd, 0, pct);
+/*
+  intake.stop(brake);
+  conveyorBelt.stop(brake);*/
+  wait(300, msec);
+   GyroTurn(172.5);
+   PinchDrive(43);
+  conveyorBelt.spin(fwd, 100, pct);
+  /*wait(300,msec);
+  GyroTurn(130);
+  PinchDrive(40);*/
 
 }
 
@@ -413,9 +240,85 @@ PinchDrive(108);
 void usercontrol(void){
   // User control code here, inside the loop
   while (1) {
+
+      //drive
+      display();
+
+      // drive
+      int LeftJoystick = Controller1.Axis3.position(pct);
+      int RightJoystick = Controller1.Axis2.position(pct);
+
+      time_drive(LeftJoystick, RightJoystick, 10);
+
+      // intake & conveyor belt
+      if(Controller1.ButtonR1.PRESSED){
+        intake.setVelocity(100, pct);
+        intake.spin(fwd);
+        conveyorBelt.setVelocity(100, pct);
+        conveyorBelt.spin(fwd);
+      }
+      else if(Controller1.ButtonR2.PRESSED){
+        intake.setVelocity(100, pct);
+        intake.spin(reverse);
+        conveyorBelt.setVelocity(100, pct);
+        conveyorBelt.spin(reverse);
+      }
+      else if(Controller1.ButtonR1.RELEASED or Controller1.ButtonR2.RELEASED){
+        intake.stop(brake);
+        conveyorBelt.stop(brake);
+      }
+
+        // pneumatics (mobile goal clamp)
+        if (Controller1.ButtonL2.PRESSED){
+          mogoClamp();
+        } 
+        else if (Controller1.ButtonL1.PRESSED){
+          mogoUnclamp();
+        }
+
+        if (Controller1.ButtonUp.PRESSED){
+          doinker1.set(false);
+        }
+        else if (Controller1.ButtonDown.PRESSED){
+          doinker1.set(true); 
+        }
+        // delete comments up to here
+        
+        /*if(Controller1.ButtonL1.pressing()){
+         clamp1.set(true);
+      }
+      else if(Controller1.ButtonL2.pressing()){
+       clamp1.set(false);
+      }
+      */
+      
+      // lady brown
+      if(Controller1.ButtonB.pressing()){
+        ladybrown.spin(fwd, 100, pct);
+        ladybrown2.spin(fwd, 100, pct);
+      }
+      else if(Controller1.ButtonX.pressing()){
+        ladybrown.spin(reverse, 100, pct);
+        ladybrown2.spin(reverse, 100, pct);
+      }
+      else{
+       ladybrown.stop(brake);
+       ladybrown2.stop(brake);
+      }
+
+      /*if(Controller1.ButtonA.pressing()){
+        ladybrown.spinToPosition(10, deg);
+        ladybrown2.spinToPosition(10, deg);
+      }
+      else{
+        ladybrown.stop(brake);
+        ladybrown2.stop(brake);
+      }*/
     
+    
+
     wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources
+                    // prevent wwasted resources
   }
 }
 
